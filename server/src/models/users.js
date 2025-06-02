2874ef99582c3f8b389c2527aae775e2ecb9d137
Eva Rosati <evaro@MacBook-Air-4.local>
2025-06-02 11:26:10 -0400

    created user model for authentication

--- server/src/models/users.js
+++ server/src/models/users.js
@@ -19,6 +19,22 @@ const userSchema = new mongoose.Schema({
             message : 'Please select correct role for user'
         },
         default : 'user'
-    }
+    },
+    password : {
+        type : String, 
+        required : [true, "Please enter a password for your account"],
+        minlength : [8, 'Your password must be at least 8 characters long'],
+        select : false //hide password
+    },
+    createdAt : {
+        type : Date, 
+        default : Date.now
+    }, 
+    resetPasswordToken : String, 
+    resetPasswordExprire : Date
 
 });
+
+module.exports = mongoose.model('User', userSchema);
+// this will create a collection called users in the database
+// if the collection already exists, it will use the existing collection
